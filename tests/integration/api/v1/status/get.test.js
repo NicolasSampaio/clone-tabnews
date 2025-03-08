d26640b2a32079958cb83b47a6058023036e9670
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2025-03-08 16:30:28 -0300

    Refactor database query handling and enhance status API response with detailed PostgreSQL connection information

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,17 +1,19 @@
-test("GET to api/v1/status returns 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
-  expect(responseBody.versao_postgres).toBeDefined();
-  expect(responseBody.versao_postgres).toMatch(/^PostgreSQL/);
-  expect(responseBody.conexoes_maximas).toBeDefined();
-  expect(responseBody.conexoes_maximas).toMatch(/^\d+$/);
-  expect(responseBody.conexoes_usadas).toBeDefined();
-  expect(responseBody.conexoes_usadas).toMatch(/^\d+$/);
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.updated_at).toMatch(
-    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?(Z|[+-]\d{2}:\d{2})$/,
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
+
+test("Teste de SQL Injection", async () => {
+  await fetch(
+    "http://localhost:3000/api/v1/status?database_name='; SELECT pg_sleep(4); --",
   );
 });
