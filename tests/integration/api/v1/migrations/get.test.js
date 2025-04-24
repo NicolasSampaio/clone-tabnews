04e572db0a994993f63ecdbcbf193a02aae55013
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2025-04-24 06:12:59 -0300

    Revert "Merge pull request #14 from NicolasSampaio/fix-npm-test"
    This reverts commit 205519589230bb61ce15d9720e2e17ca4c8b8f1e, reversing
    changes made to a216a19201a3458ffbe452374695bb932f2e95c2.

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,11 +1,6 @@
 import database from "infra/database.js";
-import orchestrator from "tests/orchestrator.js";
 
-beforeAll(async () => {
-  await orchestrator.waitForAllServices();
-  cleanDatabase();
-});
-
+beforeAll(cleanDatabase);
 async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public");
 }
